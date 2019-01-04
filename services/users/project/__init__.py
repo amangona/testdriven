6c37955e74cf8fb34f3c9ca76cc28176f21659e6
amangona <abrahammangona@gmail.com>
2019-01-03 20:30:34 -0600

    added docker

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,28 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask,
+from flask_sqlachemy import SQLAlchemy
 
-#instantiate the app
+# instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
