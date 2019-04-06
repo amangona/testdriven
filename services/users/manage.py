994127c34b22890f8983e2710ad7bbea4daeea9f
amangona <abrahammangona@gmail.com>
2019-04-05 21:47:29 -0500

    blueprint

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,10 +1,12 @@
-from flask.cli import FlaskGroup
-
-from project import app, db
-
 import unittest
 
-cli = FlaskGroup(app)
+from flask.cli import FlaskGroup
+
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 @cli.command()
 def recreate_db():
