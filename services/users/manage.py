a7a255279e93518e4189c49c97067514cd164230
amangona <abrahammangona@gmail.com>
2019-05-01 23:51:35 -0500

    69% coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -5,8 +5,35 @@ from flask.cli import FlaskGroup
 from project import create_app, db
 from project.api.models import User
 
+import coverage
+
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
 
 @cli.command()
 def recreate_db():
